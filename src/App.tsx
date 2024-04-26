1da6d0f4b300672364d46e8c5e460926d3011c0e
okayu.f <85050514+yu-fran@users.noreply.github.com>
2024-04-27 05:26:25 +0900

    formatterの適用

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import { TextField } from "@mui/material";
-import TextareaAutosize from '@mui/material/TextareaAutosize';
+import TextareaAutosize from "@mui/material/TextareaAutosize";
 
 const Calender: React.FC = () => {
   const days: number[] = [...Array(31).keys()];
@@ -12,13 +12,27 @@ const Calender: React.FC = () => {
     <table>
       <thead>
         <Grid container columns={7}>
-          <Grid item xs={1}><Typography>日</Typography></Grid>
-          <Grid item xs={1}><Typography>月</Typography></Grid>
-          <Grid item xs={1}><Typography>火</Typography></Grid>
-          <Grid item xs={1}><Typography>水</Typography></Grid>
-          <Grid item xs={1}><Typography>木</Typography></Grid>
-          <Grid item xs={1}><Typography>金</Typography></Grid>
-          <Grid item xs={1}><Typography>土</Typography></Grid>
+          <Grid item xs={1}>
+            <Typography>日</Typography>
+          </Grid>
+          <Grid item xs={1}>
+            <Typography>月</Typography>
+          </Grid>
+          <Grid item xs={1}>
+            <Typography>火</Typography>
+          </Grid>
+          <Grid item xs={1}>
+            <Typography>水</Typography>
+          </Grid>
+          <Grid item xs={1}>
+            <Typography>木</Typography>
+          </Grid>
+          <Grid item xs={1}>
+            <Typography>金</Typography>
+          </Grid>
+          <Grid item xs={1}>
+            <Typography>土</Typography>
+          </Grid>
         </Grid>
         {[...Array(6)].map((_, index) => (
           <Week key={index} days={days.slice(index * 7, (index + 1) * 7)} />
@@ -35,13 +49,27 @@ interface WeekProps {
 const Week: React.FC<WeekProps> = (props) => {
   return (
     <Grid container columns={7} spacing={0}>
-      <Grid item xs={1}><Day day={props.days[0]} /></Grid>
-      <Grid item xs={1}><Day day={props.days[1]} /></Grid>
-      <Grid item xs={1}><Day day={props.days[2]} /></Grid>
-      <Grid item xs={1}><Day day={props.days[3]} /></Grid>
-      <Grid item xs={1}><Day day={props.days[4]} /></Grid>
-      <Grid item xs={1}><Day day={props.days[5]} /></Grid>
-      <Grid item xs={1}><Day day={props.days[6]} /> </Grid>
+      <Grid item xs={1}>
+        <Day day={props.days[0]} />
+      </Grid>
+      <Grid item xs={1}>
+        <Day day={props.days[1]} />
+      </Grid>
+      <Grid item xs={1}>
+        <Day day={props.days[2]} />
+      </Grid>
+      <Grid item xs={1}>
+        <Day day={props.days[3]} />
+      </Grid>
+      <Grid item xs={1}>
+        <Day day={props.days[4]} />
+      </Grid>
+      <Grid item xs={1}>
+        <Day day={props.days[5]} />
+      </Grid>
+      <Grid item xs={1}>
+        <Day day={props.days[6]} />
+      </Grid>
     </Grid>
   );
 };
@@ -51,13 +79,17 @@ interface DayProps {
 }
 
 const Day: React.FC<DayProps> = (props) => {
-  return <Button variant="text" sx={{width: '100%', minWidth: "0px"}} >{props.day}</Button>;
-}
+  return (
+    <Button variant="text" sx={{ width: "100%", minWidth: "0px" }}>
+      {props.day}
+    </Button>
+  );
+};
 
 function App() {
   return (
     <>
-      <Typography variant="h5" component="h1" sx={{mb: 4}}>
+      <Typography variant="h5" component="h1" sx={{ mb: 4 }}>
         日程調整メーカー
       </Typography>
       <Grid container spacing={2}>
@@ -67,13 +99,19 @@ function App() {
         <Grid item xs={6}>
           <Grid container direction="column" spacing={2}>
             <Grid item>
-              <TextField multiline minRows={8}>日程調整メーカー</TextField>
+              <TextField multiline minRows={8}>
+                日程調整メーカー
+              </TextField>
             </Grid>
             <Grid item xs={8}>
-              <Button variant="contained" fullWidth>copy</Button>
+              <Button variant="contained" fullWidth>
+                copy
+              </Button>
             </Grid>
             <Grid item>
-              <Button variant="outlined" fullWidth>text</Button>
+              <Button variant="outlined" fullWidth>
+                text
+              </Button>
             </Grid>
           </Grid>
         </Grid>
