394e02febf2063b0c4d27aca81f84a248c23b3d7
okayu.f <85050514+yu-fran@users.noreply.github.com>
2024-04-26 15:35:55 +0900

    muiを利用したスタイルの調整大枠（途中）

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,83 @@
 import "./App.css";
-// import { FC } from 'react';
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import { TextField } from "@mui/material";
+import TextareaAutosize from '@mui/material/TextareaAutosize';
 
 const Calender: React.FC = () => {
   const days: number[] = [...Array(31).keys()];
   console.log(days);
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>日</th>
-            <th>月</th>
-            <th>火</th>
-            <th>水</th>
-            <th>木</th>
-            <th>金</th>
-            <th>土</th>
-          </tr>
-          {[...Array(6)].map((_, index) => (
-            <Week key={index} days={days.slice(index * 7, (index + 1) * 7)} />
-          ))}
-        </thead>
-      </table>
-    </div>
+    <table>
+      <thead>
+        <Grid container columns={7}>
+          <Grid item xs={1}><Typography>日</Typography></Grid>
+          <Grid item xs={1}><Typography>月</Typography></Grid>
+          <Grid item xs={1}><Typography>火</Typography></Grid>
+          <Grid item xs={1}><Typography>水</Typography></Grid>
+          <Grid item xs={1}><Typography>木</Typography></Grid>
+          <Grid item xs={1}><Typography>金</Typography></Grid>
+          <Grid item xs={1}><Typography>土</Typography></Grid>
+        </Grid>
+        {[...Array(6)].map((_, index) => (
+          <Week key={index} days={days.slice(index * 7, (index + 1) * 7)} />
+        ))}
+      </thead>
+    </table>
   );
-}
+};
 
-type WeekProps = {
+interface WeekProps {
   days: number[];
 }
 
 const Week: React.FC<WeekProps> = (props) => {
   return (
-    <tr>
-      <td>{props.days[0]}</td>
-      <td>{props.days[1]}</td>
-      <td>{props.days[2]}</td>
-      <td>{props.days[3]}</td>
-      <td>{props.days[4]}</td>
-      <td>{props.days[5]}</td>
-      <td>{props.days[6]}</td>
-    </tr>
+    <Grid container columns={7} spacing={0}>
+      <Grid item xs={1}><Day day={props.days[0]} /></Grid>
+      <Grid item xs={1}><Day day={props.days[1]} /></Grid>
+      <Grid item xs={1}><Day day={props.days[2]} /></Grid>
+      <Grid item xs={1}><Day day={props.days[3]} /></Grid>
+      <Grid item xs={1}><Day day={props.days[4]} /></Grid>
+      <Grid item xs={1}><Day day={props.days[5]} /></Grid>
+      <Grid item xs={1}><Day day={props.days[6]} /> </Grid>
+    </Grid>
   );
+};
+
+interface DayProps {
+  day: number;
+}
+
+const Day: React.FC<DayProps> = (props) => {
+  return <Button variant="text" sx={{width: '100%', minWidth: "0px"}} >{props.day}</Button>;
 }
 
 function App() {
   return (
     <>
-      <h1>以下の日程でご都合いかがでしょうかメーカー</h1>
-      <Calender />
-      <div>
-        <textarea>以下の日程でご都合いかがでしょうか</textarea>
-        <button>copy</button>
-        <button>reset</button>
-      </div>
+      <Typography variant="h5" component="h1" sx={{mb: 4}}>
+        日程調整メーカー
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Calender />
+        </Grid>
+        <Grid item xs={6}>
+          <Grid container direction="column" spacing={2}>
+            <Grid item>
+              <TextField multiline minRows={8}>日程調整メーカー</TextField>
+            </Grid>
+            <Grid item xs={8}>
+              <Button variant="contained" fullWidth>copy</Button>
+            </Grid>
+            <Grid item>
+              <Button variant="outlined" fullWidth>text</Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </>
   );
 }
