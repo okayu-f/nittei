51747787cb2bea4ad6d2eb5c5da4badeb66c96b5
okayu.f <85050514+yu-fran@users.noreply.github.com>
2024-04-29 05:43:42 +0900

    各種ボタンのモックを設置、レイアウトの修正

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,66 @@ import "./App.css";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
-import { Box, Container, TextField } from "@mui/material";
+import { Container, TextField } from "@mui/material";
+
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const Calender: React.FC = () => {
-  const days: number[] = [...Array(31).keys()];
+  const days: number[] = [];
+  days.push(31);
+  days.push(...[...Array(30)].map((_, index) => index + 1));
+  days.push(...[...Array(4)].map((_, index) => index + 1));
   console.log(days);
   return (
-    <table>
-      <thead>
-        <Grid container columns={7}>
-          <Grid item xs={1}>
-            <Typography>日</Typography>
-          </Grid>
-          <Grid item xs={1}>
-            <Typography>月</Typography>
-          </Grid>
-          <Grid item xs={1}>
-            <Typography>火</Typography>
-          </Grid>
-          <Grid item xs={1}>
-            <Typography>水</Typography>
-          </Grid>
-          <Grid item xs={1}>
-            <Typography>木</Typography>
-          </Grid>
-          <Grid item xs={1}>
-            <Typography>金</Typography>
-          </Grid>
-          <Grid item xs={1}>
-            <Typography>土</Typography>
-          </Grid>
+    <>
+      <Grid container>
+        <Grid item xs={2}>
+          <Button variant="text">
+            <KeyboardArrowLeftIcon />
+          </Button>
         </Grid>
-        {[...Array(6)].map((_, index) => (
-          <Week key={index} days={days.slice(index * 7, (index + 1) * 7)} />
-        ))}
-      </thead>
-    </table>
+        <Grid item xs={2}>
+          <Button variant="text">今日</Button>
+        </Grid>
+        <Grid item xs={6}>
+          <Button variant="text">2024年4月</Button>
+        </Grid>
+        <Grid item xs={2}>
+          <Button variant="text">
+            <KeyboardArrowRightIcon />
+          </Button>
+        </Grid>
+      </Grid>
+      <Grid container columns={7}>
+        <Grid item xs={1}>
+          <Typography sx={{ fontWeight: "bold" }}>日</Typography>
+        </Grid>
+        <Grid item xs={1}>
+          <Typography sx={{ fontWeight: "bold" }}>月</Typography>
+        </Grid>
+        <Grid item xs={1}>
+          <Typography sx={{ fontWeight: "bold" }}>火</Typography>
+        </Grid>
+        <Grid item xs={1}>
+          <Typography sx={{ fontWeight: "bold" }}>水</Typography>
+        </Grid>
+        <Grid item xs={1}>
+          <Typography sx={{ fontWeight: "bold" }}>木</Typography>
+        </Grid>
+        <Grid item xs={1}>
+          <Typography sx={{ fontWeight: "bold" }}>金</Typography>
+        </Grid>
+        <Grid item xs={1}>
+          <Typography sx={{ fontWeight: "bold" }}>土</Typography>
+        </Grid>
+      </Grid>
+      {[...Array(5)].map((_, index) => (
+        <Week key={index} days={days.slice(index * 7, (index + 1) * 7)} />
+      ))}
+    </>
   );
 };
 
@@ -85,20 +109,79 @@ const Day: React.FC<DayProps> = (props) => {
   );
 };
 
+const Times: React.FC = () => {
+  const times: string[] = [
+    "10:00",
+    "10:30",
+    "11:00",
+    "11:30",
+    "12:00",
+    "12:30",
+    "13:00",
+    "13:30",
+    "14:00",
+    "14:30",
+    "15:00",
+    "15:30",
+    "16:00",
+    "16:30",
+  ];
+  return (
+    <Grid container direction="column" spacing={0}>
+      <Grid item>
+        <Button
+          variant="text"
+          sx={{ width: "50%", minWidth: "0px", height: 30 }}
+        >
+          <KeyboardArrowUpIcon />
+        </Button>
+      </Grid>
+      {times.map((time, index) => (
+        <Grid item key={index} sx={{ height: 30 }}>
+          <Button variant="text" sx={{ width: "50%", minWidth: "0px" }}>
+            {time}
+          </Button>
+        </Grid>
+      ))}
+      <Grid item>
+        <Button
+          variant="text"
+          sx={{ width: "50%", minWidth: "0px", height: 30 }}
+        >
+          <KeyboardArrowDownIcon />
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
 function App() {
   return (
-    <Box sx={{ alignItems: 'flex-start' }}>
+    <Container sx={{ minWidth: 720 }}>
       <Typography variant="h5" component="h1" sx={{ mb: 4 }}>
         日程調整メーカー
       </Typography>
       <Grid container spacing={2}>
         <Grid item xs={6}>
-          <Calender />
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+              <Calender />
+            </Grid>
+            <Grid item xs={4}>
+              <Times />
+            </Grid>
+          </Grid>
         </Grid>
         <Grid item xs={6}>
           <Grid container direction="column" spacing={2}>
             <Grid item>
-              <TextField multiline minRows={8} defaultValue={"以下の日程でご都合いかがでしょうか"}>
+              <TextField
+                multiline
+                minRows={8}
+                defaultValue={"以下の日程でご都合いかがでしょうか。"}
+                inputProps={{ style: { fontSize: 14 } }}
+                sx={{ width: "100%" }}
+              >
                 日程調整メーカー
               </TextField>
             </Grid>
@@ -115,7 +198,7 @@ function App() {
           </Grid>
         </Grid>
       </Grid>
-    </Box>
+    </Container>
   );
 }
 
