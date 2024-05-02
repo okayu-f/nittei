242ee0038e171d3f4cb5f1f338d1d407326d567d
okayu.f <85050514+yu-fran@users.noreply.github.com>
2024-05-02 15:12:40 +0900

    タイトル部とコンテナ部を切り離し

--- src/App.tsx
+++ src/App.tsx
@@ -2,57 +2,65 @@ import "./App.css";
 import Calender from "./Calender";
 import Times from "./Times";
 
+import { useState } from "react";
+import { getMonth } from "date-fns";
+
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import { Container, TextField } from "@mui/material";
 
+const Main: React.FC = () => {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={6}>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <Calender />
+          </Grid>
+          <Grid item xs={4}>
+            <Times />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={6}>
+        <Grid container direction="column" spacing={2}>
+          <Grid item>
+            <TextField
+              multiline
+              minRows={8}
+              defaultValue={"以下の日程でご都合いかがでしょうか。"}
+              inputProps={{ style: { fontSize: 14 } }}
+              sx={{ width: "100%" }}
+            >
+              日程調整メーカー
+            </TextField>
+          </Grid>
+          <Grid item xs={8}>
+            <Button variant="contained" fullWidth>
+              copy
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="outlined" fullWidth>
+              reset
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
 
-function App() {
+const App = () => {
   return (
     <Container sx={{ minWidth: 720 }}>
       <Typography variant="h5" component="h1" sx={{ mb: 4 }}>
         日程調整メーカー
       </Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <Grid container spacing={2}>
-            <Grid item xs={8}>
-              <Calender />
-            </Grid>
-            <Grid item xs={4}>
-              <Times />
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item xs={6}>
-          <Grid container direction="column" spacing={2}>
-            <Grid item>
-              <TextField
-                multiline
-                minRows={8}
-                defaultValue={"以下の日程でご都合いかがでしょうか。"}
-                inputProps={{ style: { fontSize: 14 } }}
-                sx={{ width: "100%" }}
-              >
-                日程調整メーカー
-              </TextField>
-            </Grid>
-            <Grid item xs={8}>
-              <Button variant="contained" fullWidth>
-                copy
-              </Button>
-            </Grid>
-            <Grid item>
-              <Button variant="outlined" fullWidth>
-                reset
-              </Button>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
+      <Main />
     </Container>
   );
-}
+};
 
 export default App;
